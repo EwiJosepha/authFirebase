dd0df58e772e1fc01b1f039759f2cf77da47e9cd
ewi josepha <ewijosepha@gmail.com>
2024-02-22 16:05:35 +0100

    mini social media with likes feature

--- src/Pages/main/Postt.tsx
+++ src/Pages/main/Postt.tsx
@@ -1,41 +1,74 @@
 import React, { useEffect, useState } from 'react'
 import { CreatePostList } from './Main'
-import { CollectionReference, DocumentData, addDoc, collection, query, where, getDocs } from 'firebase/firestore'
+import { CollectionReference, DocumentData, addDoc, collection, query, where, getDocs, deleteDoc, doc } from 'firebase/firestore'
 import { auth, db } from '../../config/firebase'
 import { useAuthState } from 'react-firebase-hooks/auth'
 
 interface Props {
-  post: CreatePostList 
+  post: CreatePostList
 }
 
 interface Like {
-  userId : string
+  likeId: string
+  userId: string
 }
 
 
 function Post(props: Props) {
-  const[like, setLike] = useState<Like[] | null>(null)
-  const{post} = props;
-  const [user]= useAuthState(auth)
-  const likesRef =collection(db, "likes");
+  const [like, setLike] = useState<Like[] | null>(null)
+  const { post } = props;
+  const [user] = useAuthState(auth)
+  const likesRef = collection(db, "likes");
   const likesDoc = query(likesRef, where("postId", "==", post.id))
-const getlikes = async() =>{
- const data= await getDocs(likesDoc)
- setLike(data.docs.map((doc)=>({userId: doc.data().userId})));
- 
-}
+  const getlikes = async () => {
+    const data = await getDocs(likesDoc)
+    setLike(data.docs.map((doc) => ({ userId: doc.data().userId, likeId: doc.id })));
 
-useEffect(()=>{
-  getlikes()
-},[])
+  }
+
+  useEffect(() => {
+    getlikes()
+  }, [])
+
+
   async function addLikes() {
-  await addDoc (likesRef, {userId:user?.uid, postId: post.id})
-  
-}
+    try {
+     const newDoc =  await addDoc(likesRef, { userId: user?.uid, postId: post.id })
+      if (user) {
+        setLike((prev) => prev ? [...prev, { userId: user?.uid, likeId: newDoc.id }] : [{ userId: user?.uid }])
+      }
+    } catch (er) {
+      console.log(er);
 
-const hasuserLiked = like?.find((lik)=>{
-  lik.userId == user?.uid
-})
+    }
+
+  }
+
+  async function removeLikes() {
+   
+    try {
+      const liketoDeletequerry = query(likesRef, where("postId", "==", post.id), where("userId", "==", user?.uid))
+      const deleteddata = await getDocs(liketoDeletequerry)
+      const likeId = deleteddata.docs[0].id
+      const likeToDelete= doc(db, "likes", likeId)
+      await deleteDoc(likeToDelete)
+     
+      if (user) {
+       setLike((prev)=> prev?.filter((like)=>(
+like.id === likeId
+       )))
+      }
+    } catch (er) {
+      console.log(er);
+
+    }
+
+  }
+
+
+const hasuserLiked = like?.find((lik) => (
+    lik.userId == user?.uid
+  ))
 
   const styles = {
     margin: "0 auto",
@@ -45,7 +78,7 @@ const hasuserLiked = like?.find((lik)=>{
     paddimg: "30px",
     marginTop: "40px"
 
-}
+  }
   return (
     <div style={styles}>
       <div className="title">
@@ -56,8 +89,10 @@ const hasuserLiked = like?.find((lik)=>{
       </div>
       <div className="footer">
         <p>@{post.username}</p>
-        <button onClick={addLikes}>{hasuserLiked ?  <>#128078;</> : <>#128077;</>}</button>
-          {like && <p> Likes:{like?.length} </p>}
+        <button onClick={hasuserLiked ? removeLikes: addLikes}>
+          {hasuserLiked ? <>&#128078;</> : <>&#128077;</>}{""}
+          </button>
+        {like && <p> Likes:{like?.length} </p>}
       </div>
     </div>
   )
